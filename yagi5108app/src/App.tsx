9cf6b3f0f04d08c6ea29dd73d670bf5acb7e460f
suzuki hayato <hayahayawrok@gmail.com>
2023-06-27 00:48:07 +0900

    init

--- yagi5108app/src/App.tsx
+++ yagi5108app/src/App.tsx
@@ -1,6 +1,33 @@
-function App() {
-  return (
-  )
-}
+import { Container, Box } from '@mui/material';
+import bgImage from '../image/背景.png';
 
-export default App
+const App = () => {
+  return (
+    <Box
+      style={{
+        backgroundColor: 'black',
+        width: '100vw',
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <Container
+        maxWidth="sm"
+        disableGutters
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          height: '100%',
+          maxHeight: '100vh', 
+        }}
+      >
+        <img src={bgImage} alt="" style={{ width: 'auto', height: 'auto', maxHeight: '100%' }} />
+      </Container>
+    </Box>
+  );
+};
+
+export default App;
