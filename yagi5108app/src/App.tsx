0103c18a9f0ac93a860c00284c0407c7970dc786
MinamizonoEno <a02142211@gmail.com>
2023-06-27 10:43:07 +0900

    遊び方を追加

--- yagi5108app/src/App.tsx
+++ yagi5108app/src/App.tsx
@@ -1,6 +1,13 @@
+import React from "react";
+import Tutorial from "./Tutorial";
 function App() {
+  const [open, setOpen] = React.useState(true);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
   return (
-  )
+    <Tutorial open={open} openclick={handleOpen} closeclick={handleClose} />
+  );
 }
 
-export default App
+export default App;
