87db31aa1c4477cfd45dcc74cf77598297993b09
MinamizonoEno <a02142211@gmail.com>
2023-06-30 20:55:29 +0900

    ヤギのアニメーション追加

--- yagi5108app/src/RandomWalker.tsx
+++ yagi5108app/src/RandomWalker.tsx
@@ -1,39 +1,20 @@
-<<<<<<< HEAD
 import React, { useRef, useEffect, useState } from "react";
 import yagi_left from "../image/yagi_left.png";
 import yagi_right from "../image/yagi_right.png";
-=======
-// Reactと関連するモジュールをインポート
-import React, { useRef, useEffect, useState } from 'react';
-// 画像ファイルをインポート
-import image1 from '../image/yagi_syokuzi.png';
->>>>>>> e77d824b4e623d2cafce5106da9e3326afdcc4ad
 
-// ランダムに移動するコンポーネントの定義
 const RandomWalker: React.FC = () => {
-<<<<<<< HEAD
   const walkerRef = useRef<HTMLDivElement>(null);
   const [position, setPosition] = useState({ x: 100, y: 300 });
   const [containerSize, setContainerSize] = useState({ width: 0, height: 0 });
   const [rangeVisible, setRangeVisible] = useState(false);
-=======
-  // 画像要素の参照を作成
-  const walkerRef = useRef<HTMLDivElement | null>(null);
-  // 画像の位置を管理する状態変数を宣言
-  const [position, setPosition] = useState({ x: 0, y: 0 });
->>>>>>> e77d824b4e623d2cafce5106da9e3326afdcc4ad
 
-  // コンポーネントがマウントされた時に実行される副作用フック
   useEffect(() => {
-    // 画像要素の参照を取得
     const walkerElement = walkerRef.current;
     if (!walkerElement) return;
 
-    // 画像要素の親要素（コンテナ）を取得
     const containerElement = walkerElement.parentElement;
     if (!containerElement) return;
 
-<<<<<<< HEAD
     const updateContainerSize = () => {
       const containerWidth = containerElement.clientWidth;
       const containerHeight = containerElement.clientHeight;
@@ -57,38 +38,16 @@ const RandomWalker: React.FC = () => {
     const startY = containerSize.height / 2 - 150; // 開始位置のy座標
     const endX = containerSize.width / 2 + 150; // 終了位置のx座標
     const endY = containerSize.height / 2 + 150; // 終了位置のy座標
-=======
-    // コンテナの幅と高さを取得
-    const containerWidth = containerElement.clientWidth;
-    const containerHeight = containerElement.clientHeight;
 
-    const centerX = containerWidth / 2 - walkerElement.clientWidth / 2; // 中央位置の修正
-    const centerY = containerHeight / 2; // 画面の高さの中央
->>>>>>> e77d824b4e623d2cafce5106da9e3326afdcc4ad
-
-    // 開始位置と終了位置の計算
-    const startX = centerX - walkerElement.clientWidth / 2; // 開始位置のx座標
-    const startY = centerY - walkerElement.clientHeight / 2; // 開始位置のy座標
-    const endX = centerX + walkerElement.clientWidth / 2; // 終了位置のx座標
-    const endY = centerY + walkerElement.clientHeight / 2; // 終了位置のy座標
-
-    // 位置を更新する関数の定義
     const updatePosition = () => {
       const newX = startX + Math.random() * (endX - startX);
       const newY = startY + Math.random() * (endY - startY);
 
-      // 位置を設定
       setPosition({ x: newX, y: newY });
     };
 
-<<<<<<< HEAD
     const intervalId = setInterval(updatePosition, 3000); // 移動アニメーションの時間を延長（2000ミリ秒）
-=======
-    // 一定間隔で位置を更新するタイマーを設定
-    const intervalId = setInterval(updatePosition, 1000);
->>>>>>> e77d824b4e623d2cafce5106da9e3326afdcc4ad
 
-    // コンポーネントのアンマウント時にタイマーをクリア
     return () => {
       clearInterval(intervalId);
     };
@@ -101,7 +60,6 @@ const RandomWalker: React.FC = () => {
   const backgroundImage =
     position.x > containerSize.width / 2 ? yagi_right : yagi_left;
 
-  // 画像要素をレンダリング
   return (
     <div
       style={{
@@ -146,5 +104,4 @@ const RandomWalker: React.FC = () => {
   );
 };
 
-// コンポーネントをエクスポート
 export default RandomWalker;
