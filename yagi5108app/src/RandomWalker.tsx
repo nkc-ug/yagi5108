e77d824b4e623d2cafce5106da9e3326afdcc4ad
suzukihaya <hayahayawork@gmail.com>
2023-06-30 12:45:08 +0900

    ヤギ移動

--- yagi5108app/src/RandomWalker.tsx
+++ yagi5108app/src/RandomWalker.tsx
@@ -1,39 +1,57 @@
+// Reactと関連するモジュールをインポート
 import React, { useRef, useEffect, useState } from 'react';
+// 画像ファイルをインポート
 import image1 from '../image/yagi_syokuzi.png';
 
+// ランダムに移動するコンポーネントの定義
 const RandomWalker: React.FC = () => {
+  // 画像要素の参照を作成
   const walkerRef = useRef<HTMLDivElement | null>(null);
+  // 画像の位置を管理する状態変数を宣言
   const [position, setPosition] = useState({ x: 0, y: 0 });
 
+  // コンポーネントがマウントされた時に実行される副作用フック
   useEffect(() => {
+    // 画像要素の参照を取得
     const walkerElement = walkerRef.current;
     if (!walkerElement) return;
 
+    // 画像要素の親要素（コンテナ）を取得
     const containerElement = walkerElement.parentElement;
     if (!containerElement) return;
 
+    // コンテナの幅と高さを取得
     const containerWidth = containerElement.clientWidth;
     const containerHeight = containerElement.clientHeight;
 
-    const startX = containerWidth / 4 - walkerElement.clientWidth / 2; // 開始位置のx座標
-    const startY = containerHeight / 4 - walkerElement.clientHeight / 2; // 開始位置のy座標
-    const endX = containerWidth * 3 / 4 - walkerElement.clientWidth / 2; // 終了位置のx座標
-    const endY = containerHeight * 3 / 4 - walkerElement.clientHeight / 2; // 終了位置のy座標
+    const centerX = containerWidth / 2 - walkerElement.clientWidth / 2; // 中央位置の修正
+    const centerY = containerHeight / 2; // 画面の高さの中央
 
+    // 開始位置と終了位置の計算
+    const startX = centerX - walkerElement.clientWidth / 2; // 開始位置のx座標
+    const startY = centerY - walkerElement.clientHeight / 2; // 開始位置のy座標
+    const endX = centerX + walkerElement.clientWidth / 2; // 終了位置のx座標
+    const endY = centerY + walkerElement.clientHeight / 2; // 終了位置のy座標
+
+    // 位置を更新する関数の定義
     const updatePosition = () => {
       const newX = startX + Math.random() * (endX - startX);
       const newY = startY + Math.random() * (endY - startY);
 
+      // 位置を設定
       setPosition({ x: newX, y: newY });
     };
 
+    // 一定間隔で位置を更新するタイマーを設定
     const intervalId = setInterval(updatePosition, 1000);
 
+    // コンポーネントのアンマウント時にタイマーをクリア
     return () => {
       clearInterval(intervalId);
     };
   }, []);
 
+  // 画像要素をレンダリング
   return (
     <div
       ref={walkerRef}
@@ -51,4 +69,5 @@ const RandomWalker: React.FC = () => {
   );
 };
 
+// コンポーネントをエクスポート
 export default RandomWalker;
