0e7f0fe6ae6d99873c6172a38b3baae0a44daadd
suzukihaya <hayahayawork@gmail.com>
2023-06-30 11:49:43 +0900

    背景設定

--- yagi5108app/src/App.tsx
+++ yagi5108app/src/App.tsx
@@ -11,7 +11,7 @@ const App: React.FC = () => {
   const toggleBGM = () => {
     if (audioRef.current) {
       if (isPlaying) {
-        audioRef.current.pause();
+        audioRef.current.pause()
       } else {
         audioRef.current.play();
       }
