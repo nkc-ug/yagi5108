635bbbacaa5fec1c2ffdd505ad79a82bdca48e50
MinamizonoEno <a02142211@gmail.com>
2023-07-01 17:18:45 +0900

    画像の軽量化

--- src/App.tsx
+++ src/App.tsx
@@ -29,97 +29,30 @@ const App: React.FC = () => {
 
   return (
     <div>
-      <Box
+      <Container
+        maxWidth="sm"
+        disableGutters
         style={{
-          backgroundColor: 'black',
-          width: '100vw',
-          height: '100vh',
           display: 'flex',
           justifyContent: 'center',
           alignItems: 'center',
+          height: '100%',
+          maxHeight: '100vh',
+          position: 'relative',
+          backgroundImage: `url(${bgImage})`,
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
         }}
-        position="relative"
       >
-        <Container
-          maxWidth="sm"
-          disableGutters
-          style={{
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            height: '100%',
-            maxHeight: '100vh',
-            position: 'relative',
-          }}
-        >
-          <img src={bgImage} alt="" style={{ width: 'auto', height: 'auto', maxHeight: '100vh' }} />
-          <Box
-            position="absolute"
-            top={0}
-            left={0}
-            right={0}
-            bottom={250}
-            display="flex"
-            justifyContent="center"
-            alignItems="center"
-          >
-            <Form inputText={inputText} handleChange={handleChange} handleSubmit={handleSubmit} />
-          </Box>
-          <Box
-            position="absolute"
-            top={250}
-            left={0}
-            right={50}
-            bottom={0}
-            display="flex"
-            justifyContent="center"
-            alignItems="center"
-          >
-            <Flower />
-          </Box>
-          <Box
-            position="absolute"
-            top={200}
-            left={0}
-            right={100}
-            bottom={0}
-            display="flex"
-            justifyContent="center"
-            alignItems="center"
-          >
-            <RandomWalker />
-          </Box>
-
-          <Box
-            position="absolute"
-            top={35}
-            left={0}
-            right={40}
-            bottom={0}
-            display="flex"
-            justifyContent="center"
-            alignItems="center"
-          >
-            <SyokuziCon />
-          </Box>
-          <Box
-            position="absolute"
-            top={630}
-            left={0}
-            right={0}
-            bottom={0}
-            display="flex"
-            justifyContent="center"
-            alignItems="center"
-          ></Box>
-          <Box style={{ position: 'absolute', top: 5, left: 500 }}>
-            <BGMPlayer src={bgm} />
-          </Box>
-          <div style={{ position: 'absolute', top: 5, left: 0 }}>
-            <Tutorial open={open} openclick={handleOpen} closeclick={handleClose} />
-          </div>
-        </Container>
-      </Box>
+        <Box sx={{ height: '100vh' }}>
+          <Form inputText={inputText} handleChange={handleChange} handleSubmit={handleSubmit} />
+          <Flower />
+          <RandomWalker />
+          <SyokuziCon />
+          <BGMPlayer src={bgm} />
+          <Tutorial open={open} openclick={handleOpen} closeclick={handleClose} />
+        </Box>
+      </Container>
     </div>
   );
 };
