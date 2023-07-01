dbc8f9957792ae075aae85243311087327e7e7ad
MinamizonoEno <a02142211@gmail.com>
2023-07-01 13:58:19 +0900

    Bgmボタンの追加

--- src/App.tsx
+++ src/App.tsx
@@ -112,11 +112,11 @@ const App: React.FC = () => {
             justifyContent="center"
             alignItems="center"
           ></Box>
-
+          <Box style={{ position: 'absolute', top: 5, left: 500 }}>
+            <BGMPlayer src={bgm} />
+          </Box>
           <div style={{ position: 'absolute', top: 5, left: 0 }}>
             <Tutorial open={open} openclick={handleOpen} closeclick={handleClose} />
-            <SettingCon />
-            <BGMPlayer src={bgm} />
           </div>
         </Container>
       </Box>
