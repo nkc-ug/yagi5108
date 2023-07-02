8c901aa0050004bb8a476c19aedcd9ed85f26685
Yuma Satake <satous.y@icloud.com>
2023-07-02 10:49:54 +0900

    feat: design変更

--- src/Bgm.tsx
+++ src/Bgm.tsx
@@ -1,19 +1,10 @@
 import { useRef, useState } from 'react';
-import { Button } from '@mui/material';
+import { IconButton } from '@mui/material';
 import MusicNoteIcon from '@mui/icons-material/MusicNote';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
 
 interface BGMPlayerProps {
   src: string;
 }
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: '#FFFFFF',
-      contrastText: '#e9e4d4',
-    },
-  },
-});
 
 const BGMPlayer: React.FC<BGMPlayerProps> = ({ src }) => {
   const audioRef = useRef<HTMLAudioElement | null>(null);
@@ -33,11 +24,9 @@ const BGMPlayer: React.FC<BGMPlayerProps> = ({ src }) => {
   return (
     <>
       <audio ref={audioRef} src={src} loop />
-      <ThemeProvider theme={theme}>
-        <Button onClick={toggleBGM}>
-          <MusicNoteIcon />
-        </Button>
-      </ThemeProvider>
+      <IconButton onClick={toggleBGM}>
+        <MusicNoteIcon fontSize="large" color="primary" />
+      </IconButton>
     </>
   );
 };
