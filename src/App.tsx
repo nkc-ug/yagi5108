6700b41e224cce31faef5436611f2eabe099ff22
MinamizonoEno <a02142211@gmail.com>
2023-07-02 12:20:16 +0900

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,141 +1,142 @@
-  import React, { useEffect, useState } from 'react';
-  import { Container, Stack, Grid, Box } from '@mui/material';
-  import Tutorial from './Tutorial';
-  import Form from './Form';
-  import Flower from './Flower';
-  import bgImage from './assets/backGround.png';
-  import RandomWalker from './RandomWalker';
-  import BGMPlayer from './Bgm';
-  import bgm from './Audio/Bgm.mp3';
-  import EmotionApi from './EmotionApi';
-  import Revolution from './Revolution';
-  import Popup from './Popup';
-  import Revopopup from './Revopopup'
-  const App: React.FC = () => {
-    const [open, setOpen] = useState(true);
-    const [pop, handlepop] = useState(true);
-    const [eat, handleeat] = useState(false);
-    const handleOpen = () => setOpen(true);
-    const handleClose = () => setOpen(false);
-    const [showImage, setShowImage] = useState(false);
-    //食べた回数と進化先の変数の追加(eatCount,typeId)
-    const [inputText, setInputText] = useState('');
-    const [eatCount, setEatCount] = useState(1);
-    const [typeId, setTypeId] = useState(-1);
-    const [revopopup,setrevopopup] = useState(false); //追加
-    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-      setInputText(event.target.value);
-    };
-
-    //追加
-    //emotionData管理用のtypeの追加
-    type emotionDataType = {
-      happy: number;
-      anger: number;
-      sad: number;
-      enjoyable: number;
-      emoId: number;
-    };
-    const [emotionData, setEmotionData] = useState<emotionDataType>({
-      happy: 0,
-      anger: 0,
-      sad: 0,
-      enjoyable: 0,
-      emoId: 0,
-    });
-    const handleSubmit = async () => {
-      setEmotionData(await EmotionApi(inputText, emotionData));
-      handleGrass();
-      setInputText('');
-      handlepop(false);
-
-      if(eatCount >= 5){
-        setrevopopup(true);
-      }
-
-    };
-    const popSubmit = () => {
-      handlepop(true);
-      handleeat(true);
-    };
-    const walking = () => {
-      handleeat(false);
-    };
-    //草生成用のハンドルを追加(食事回数と条件達成で進化先の分析)
-    const handleGrass = () => {
-      console.log('草生成用');
-      setEatCount(eatCount + 1);
-      if (eatCount >= 5) {
-        setTypeId(Revolution(emotionData));
-      }
-    };
-
-    useEffect(() => {
-      if (eat) {
-        const timer = setTimeout(() => {
-          setShowImage(false);
-          walking();
-        }, 2000);
-
-        // クリーンアップ関数を返すことで、タイマーがキャンセルされる
-        return () => clearTimeout(timer);
-      } else {
-        // eatがfalseの場合はタイマーをキャンセルする
-        setShowImage(true);
-      }
-    }, [eat]);
-
-    return (
-      <div>
-        <Stack direction="row" justifyContent="center">
-          <Container disableGutters maxWidth="sm" style={{ position: 'absolute' }}>
-            <img
-              src={bgImage}
-              style={{
-                height: '90vh',
-                width: '100%',
-                objectFit: 'cover',
-              }}
-            />
-          </Container>
-          <Container disableGutters maxWidth="sm" style={{ zIndex: 1 }}>
-            <Grid container>
-              <Grid item xs={2}>
-                <Tutorial open={open} openclick={handleOpen} closeclick={handleClose} />
-              </Grid>
-              <Grid item xs={8}></Grid>
-              <Grid item xs={2}>
-                <BGMPlayer src={bgm} />
-              </Grid>
-            </Grid>
-            <Grid container>
-              <Grid item xs={1}></Grid>
-              <Grid item xs={10}>
-                <Form inputText={inputText} handleChange={handleChange} handleSubmit={handleSubmit} />
-              </Grid>
-              <Grid item xs={1}></Grid>
-            </Grid>
-            <Grid container>
-              <Grid item xs={3}></Grid>
-              <Grid item xs={6}>
-                <Flower emotionData={emotionData} eat={eat} showImage={showImage} />
-              </Grid>
-              <Grid item xs={3}></Grid>
-            </Grid>
-            <Grid container>
-              <Grid item xs={2} bgcolor="yellow"> {/*表示タイミング調整必須*/}
-                <Popup emotionData={emotionData} pop={pop} popSubmit={popSubmit} />    
-                {revopopup && <Revopopup eatCount={eatCount} />} {/*鈴木追加*/}
-              </Grid>
-              <Grid item xs={6} bgcolor="red">
-                <RandomWalker />
-              </Grid>
-              <Grid item xs={4} bgcolor="blue"></Grid>
-            </Grid>
-          </Container>
-        </Stack>
-      </div>
-    );
+import React, { useEffect, useState } from 'react';
+import { Container, Stack, Grid, Box } from '@mui/material';
+import Tutorial from './Tutorial';
+import Form from './Form';
+import Flower from './Flower';
+import bgImage from './assets/backGround.png';
+import RandomWalker from './RandomWalker';
+import BGMPlayer from './Bgm';
+import bgm from './Audio/Bgm.mp3';
+import EmotionApi from './EmotionApi';
+import Revolution from './Revolution';
+import Popup from './Popup';
+import Revopopup from './Revopopup';
+const App: React.FC = () => {
+  const [open, setOpen] = useState(true);
+  const [pop, handlepop] = useState(true);
+  const [eat, handleeat] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  const [showImage, setShowImage] = useState(false);
+  //食べた回数と進化先の変数の追加(eatCount,typeId)
+  const [inputText, setInputText] = useState('');
+  const [eatCount, setEatCount] = useState(1);
+  const [typeId, setTypeId] = useState(-1);
+  const [revopopup, setrevopopup] = useState(false); //追加
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputText(event.target.value);
   };
 
-  export default App;
+  //追加
+  //emotionData管理用のtypeの追加
+  type emotionDataType = {
+    happy: number;
+    anger: number;
+    sad: number;
+    enjoyable: number;
+    emoId: number;
+  };
+  const [emotionData, setEmotionData] = useState<emotionDataType>({
+    happy: 0,
+    anger: 0,
+    sad: 0,
+    enjoyable: 0,
+    emoId: 0,
+  });
+  const handleSubmit = async () => {
+    setEmotionData(await EmotionApi(inputText, emotionData));
+    handleGrass();
+    setInputText('');
+    handlepop(false);
+
+    if (eatCount >= 5) {
+      setrevopopup(true);
+    }
+  };
+  const popSubmit = () => {
+    handlepop(true);
+    handleeat(true);
+  };
+  const walking = () => {
+    handleeat(false);
+  };
+  //草生成用のハンドルを追加(食事回数と条件達成で進化先の分析)
+  const handleGrass = () => {
+    console.log('草生成用');
+    setEatCount(eatCount + 1);
+    if (eatCount >= 5) {
+      setTypeId(Revolution(emotionData));
+    }
+  };
+
+  useEffect(() => {
+    if (eat) {
+      const timer = setTimeout(() => {
+        setShowImage(false);
+        walking();
+      }, 2000);
+
+      // クリーンアップ関数を返すことで、タイマーがキャンセルされる
+      return () => clearTimeout(timer);
+    } else {
+      // eatがfalseの場合はタイマーをキャンセルする
+      setShowImage(true);
+    }
+  }, [eat]);
+
+  return (
+    <div>
+      <Stack direction="row" justifyContent="center">
+        <Container disableGutters maxWidth="sm" style={{ position: 'absolute' }}>
+          <img
+            src={bgImage}
+            style={{
+              height: '90vh',
+              width: '100%',
+              objectFit: 'cover',
+            }}
+          />
+        </Container>
+        <Container disableGutters maxWidth="sm" style={{ zIndex: 1 }}>
+          <Grid container>
+            <Grid item xs={2}>
+              <Tutorial open={open} openclick={handleOpen} closeclick={handleClose} />
+            </Grid>
+            <Grid item xs={8}></Grid>
+            <Grid item xs={2}>
+              <BGMPlayer src={bgm} />
+            </Grid>
+          </Grid>
+          <Grid container>
+            <Grid item xs={1}></Grid>
+            <Grid item xs={10}>
+              <Form inputText={inputText} handleChange={handleChange} handleSubmit={handleSubmit} />
+            </Grid>
+            <Grid item xs={1}></Grid>
+          </Grid>
+          <Grid container>
+            <Grid item xs={3}></Grid>
+            <Grid item xs={6}>
+              <Flower emotionData={emotionData} eat={eat} showImage={showImage} />
+            </Grid>
+            <Grid item xs={3}></Grid>
+          </Grid>
+          <Grid container>
+            <Grid item xs={2} bgcolor="yellow">
+              {' '}
+              {/*表示タイミング調整必須*/}
+              <Popup emotionData={emotionData} pop={pop} popSubmit={popSubmit} />
+              {<Revopopup eatCount={eatCount} pop={pop} />} {/*鈴木追加*/}
+            </Grid>
+            <Grid item xs={6} bgcolor="red">
+              <RandomWalker />
+            </Grid>
+            <Grid item xs={4} bgcolor="blue"></Grid>
+          </Grid>
+        </Container>
+      </Stack>
+    </div>
+  );
+};
+
+export default App;
--- src/Revopopup.tsx
+++ src/Revopopup.tsx
@@ -4,50 +4,46 @@ import { Typography } from '@mui/material';
 import Button from '@mui/material/Button';
 import yagi_blck from './assets/black_yagi.png';
 type Props = {
-  eatCount : number
-  pop : boolean
+  eatCount: number;
+  pop: boolean;
 };
 const Revopopup: FC<Props> = (props) => {
   return (
-    {props.pop ? (
-      null
-    ):(
-      <div>
-      {props.eatCount >= 5 ?(
-                <Box
-                sx={{
-                  position: 'absolute',
-                  top: '50%',
-                  left: '50%',
-                  transform: 'translate(-50%, -50%)',
-                  width: '300px',
-                  height: '300px',
-                  color: '#000',
-                  border: '2px solid #FFF',
-                  boxShadow: 24,
-                  p: 4,
-                  backgroundImage: `url(${yagi_blck})`,
-                  backgroundPosition: '-50px -300px',
-                  backgroundColor: 'brack',
-                  zIndex: 1,
-                }}
+    <div>
+      {props.pop ? (
+        <div>
+          {props.eatCount >= 5 ? (
+            <Box
+              sx={{
+                position: 'absolute',
+                top: '50%',
+                left: '50%',
+                transform: 'translate(-50%, -50%)',
+                width: '300px',
+                height: '300px',
+                color: '#000',
+                border: '2px solid #FFF',
+                boxShadow: 24,
+                p: 4,
+                backgroundImage: `url(${yagi_blck})`,
+                backgroundPosition: '-50px -300px',
+                backgroundColor: 'brack',
+                zIndex: 1,
+              }}
+            >
+              <Typography
+                id="transition-modal-description"
+                sx={{ mt: 2, fontSize: '25px' }}
+                textAlign={'center'}
               >
-           <Typography
-           id="transition-modal-description"
-           sx={{ mt: 2, fontSize: '25px' }}
-           textAlign={'center'}
-          >
-          おやおや。。ヤギの様子が....
-         </Typography>
-         <Button variant="contained" /*onClick={props.popSubmit}*/>
-         見てみる
-        </Button>
-       </Box>
-      ) : (
-        null
-      )}
+                おやおや。。ヤギの様子が....
+              </Typography>
+              <Button variant="contained" /*onClick={props.popSubmit}*/>見てみる</Button>
+            </Box>
+          ) : null}
+        </div>
+      ) : null}
     </div>
-    )}
-      );
+  );
 };
 export default Revopopup;
