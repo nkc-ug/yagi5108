0648a21eed76a97705322bf17052d52cce61df7c
Yuma Satake <satous.y@icloud.com>
2023-08-26 02:35:20 +0900

    feat: firestore追加関数

---
+++ src/api/addWordEmotions.ts
@@ -0,0 +1,28 @@
+import { addDoc, collection, doc } from 'firebase/firestore';
+import { EmotionDataType } from '../types/EmotionDataType';
+import { db } from '../lib/firebase';
+import { COLLECTIONNAME, EmotionDataDBType } from '../lib/firestore';
+
+type Props = {
+  text: string;
+  emotionData: EmotionDataType;
+};
+
+/**
+ * 文字列に対応する感情をfireStoreに追加する関数
+ *
+ * @param text 文字列
+ * @param emotionData 対応するEmotionData
+ *
+ * @see
+ * https://firebase.google.com/docs/firestore/manage-data/add-data?hl=ja
+ */
+export const addWordEmotions = async ({ text, emotionData }: Props) => {
+  const collectionRef = collection(db, COLLECTIONNAME);
+  const docData: EmotionDataDBType = {
+    documentWord: text,
+    emotionData: emotionData,
+  };
+
+  await addDoc(collectionRef, docData);
+};
