4aae10e702e0be5b55e9f7f51644d314f8da70a2
Yuma Satake <satous.y@icloud.com>
2023-08-28 21:31:01 +0900

    feat: yagiWrappeBotton2

--- src/components/EvolutionWalk.tsx
+++ src/components/EvolutionWalk.tsx
@@ -92,7 +92,6 @@ const EvolutionWalk: FC<Props> = ({ typeId, containerSize }) => {
             top: `${position.y}px`,
             transition: 'left 3s ease-in-out, top 3s ease-in-out', // 移動アニメーションの時間を延長（2秒）
           }}
-          onClick={audioPlay}
         />
       </Button>
     </div>
