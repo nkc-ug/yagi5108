bd7133b01ab23a07d24e96079a676b9c64ee147e
Yuma Satake <satous.y@icloud.com>
2023-08-28 23:47:58 +0900

    feat: eslintルール修正2

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -12,6 +12,6 @@ module.exports = {
   rules: {
     'react-refresh/only-export-components': 'warn',
     'react-hooks/exhaustive-deps': 'off',
-    'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    'no-unused-vars': ['off', { argsIgnorePattern: '^_' }],
   },
 };
