1649bbb969467296bb21899b8b220baea929af54
suzuki hayato <hayahayawrok@gmail.com>
2023-08-29 02:04:58 +0900

    モンスターの選定の修正並びにbattleに不必要なものを削除

--- src/pages/BattleView.tsx
+++ src/pages/BattleView.tsx
@@ -1,6 +1,5 @@
 import React, { FC, useEffect, useState } from 'react';
 import { Container, Stack, Grid, Box } from '@mui/material';
-import Tutorial from '../components/Tutorial';
 import Form from '../components/Form';
 import Eat from '../components/Eat';
 import NormalWalk from '../components/NormalWalk';
@@ -23,7 +22,7 @@ import mori from '../assets/mori.png';
 import { PageContainer } from '../components/PageContainer';
 import { useInput } from '../hooks/useInput';
 import { CircleProgressCon } from '../components/common/CircleProgressCon';
-import { NavBarCon } from '../components/navbar/NavBarCon';
+import { BattleNavBarCon } from '../components/battle/BattleNavBarcon';
 
 type RandomType = 0 | 1 | null;
 
@@ -54,9 +53,7 @@ export const BattleView: FC = () => {
   const [monster, setMonster] = useState<number>(0);
   const [containerSize, setContainerSize] = useState({ width: 260, height: 600 });
   const [emotionData, setEmotionData] = useState<EmotionDataType>(emotionInitialData);
-  const [isTutorialModalOpen, handleTutorialModalOpen, handleTutorialModalClose] =
-    useDiscloser(true);
-  const [isBattleModalOpen, handleBattleModalOpen, handleBattleModalClose] = useDiscloser(false);
+  const [isBattleModalOpen, handleBattleModalOpen, handleBattleModalClose] = useDiscloser(true);
   const changeRandome = () => {
     const setItem = random === 0 ? 1 : 0;
     setRandom(setItem);
@@ -182,7 +179,6 @@ export const BattleView: FC = () => {
               popSubmit={popSubmit}
               randomNum={random ?? 0}
             />
-            <Tutorial open={isTutorialModalOpen} closeClick={handleTutorialModalClose} />
             {EvoPopup ? (
               <Pulse typeId={typeId} walkEvo={WalkEvo} containerSize={containerSize} />
             ) : null}
@@ -193,7 +189,6 @@ export const BattleView: FC = () => {
                 handleChange={handleInputText}
                 handleSubmit={handleSubmit}
                 isDisableTextField={isDisableTextField()}
-                handleBattleChange={handleBattleModalOpen}
               />
               <Container
                 style={{
@@ -229,10 +224,7 @@ export const BattleView: FC = () => {
       </Stack>
 
       {/* ナビゲーションバー */}
-      <NavBarCon
-        handleTutorialModalOpen={handleTutorialModalOpen}
-        handleBattleModalOpen={handleBattleModalOpen}
-      />
+      <BattleNavBarCon handleBattleModalOpen={handleBattleModalOpen} />
 
       {/* ロード画面 */}
       <CircleProgressCon isOpen={dispCircle} />
