cb1a1dae5fb63b5de70d0918b4f5c69b75c9f3f9
suzuki hayato <hayahayawrok@gmail.com>
2023-08-31 02:40:53 +0900

    バトル用のフォームとナビバーの作成

--- src/components/battle/BattleNavbar.tsx
+++ src/components/battle/BattleNavbar.tsx
@@ -2,35 +2,20 @@ import { BottomNavigation, BottomNavigationAction, Box } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 import { FC, useRef, useState } from 'react';
 import ReplyIcon from '@mui/icons-material/Reply';
-import SearchIcon from '@mui/icons-material/Search';
-import MusicNoteIcon from '@mui/icons-material/MusicNote';
-import MusicOffIcon from '@mui/icons-material/MusicOff';
 import CoronavirusIcon from '@mui/icons-material/Coronavirus';
 import bgm from '../../Audio/Bgm.mp3';
 import { StyleMenu } from '../navbar/StyleMenu';
 
 type Props = {
-  handleBattleChange: React.MouseEventHandler<HTMLButtonElement> | undefined;
+  handleMonsterModalChange: React.MouseEventHandler<HTMLButtonElement> | undefined;
 };
 
-export const BattleNavBar: FC<Props> = ({ handleBattleChange }) => {
+export const BattleNavBar: FC<Props> = ({ handleMonsterModalChange }) => {
   const audioRef = useRef<HTMLAudioElement | null>(null);
-  const [isPlaying, setIsPlaying] = useState(false);
   const navigate = useNavigate();
 
   const [isOpenStyleMenu, setIsOpenStyleMenu] = useState(false);
   const [anchorEl, setAnchorEl] = useState<HTMLElement | null>(null);
-
-  const toggleBGM = () => {
-    if (audioRef.current) {
-      if (isPlaying) {
-        audioRef.current.pause();
-      } else {
-        audioRef.current.play();
-      }
-      setIsPlaying(!isPlaying);
-    }
-  };
 
   return (
     <Box>
@@ -52,23 +37,9 @@ export const BattleNavBar: FC<Props> = ({ handleBattleChange }) => {
         <BottomNavigationAction
           label="モンスターをみる"
           icon={<CoronavirusIcon />}
-          onClick={handleBattleChange}
-          sx={{ color: 'white' }}
-        />
-        <BottomNavigationAction
-          id="styleMenuNav"
-          label="やぎをみる"
-          icon={<SearchIcon />}
           onClick={() => {
-            setIsOpenStyleMenu(true);
-            setAnchorEl(document.getElementById('styleMenuNav'));
+            navigate('/MonsterView');
           }}
-          sx={{ color: 'white' }}
-        />
-        <BottomNavigationAction
-          label="おんがく"
-          icon={isPlaying ? <MusicOffIcon /> : <MusicNoteIcon />}
-          onClick={toggleBGM}
           sx={{ color: 'white' }}
         />
       </BottomNavigation>
